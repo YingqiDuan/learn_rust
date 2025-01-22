aecfacdd67dd867bc9da28230d8b453392e0afad
YingqiDuan <141370165+YingqiDuan@users.noreply.github.com>
2025-01-22 02:28:02 -0800

    Delete ownership/target directory

--- ownership/target/debug/ownership.d
+++
@@ -1,1 +0,0 @@
-/Users/Zhuanz1/Documents/neu_is_classes/25springclass/INFO7500/Jan_16/ownership/target/debug/ownership: /Users/Zhuanz1/Documents/neu_is_classes/25springclass/INFO7500/Jan_16/ownership/src/main.rs
